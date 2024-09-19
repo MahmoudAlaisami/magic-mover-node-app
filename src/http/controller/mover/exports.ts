eb218c08bc25d86d7bae62cc39a33e9b9be6bf16
Mahmoud Alaissami <mahmoudalaissami@gmail.com>
2024-09-20 00:29:53 +0300

    fix naming

--- src/http/controller/mover/exports.ts
+++ src/http/controller/mover/exports.ts
@@ -2,7 +2,7 @@ export { default as GetAll } from "./getAll";
 export { default as GetetByLevel } from "./getByLevel";
 export { default as GetById } from "./getById";
 export { default as Create } from "./create";
-export { default as UpdateState } from "./updateState";
+export { default as Update } from "./update";
 export { default as Upgrade } from "./upgrade";
 export { default as Downgrade } from "./downgrade";
 export { default as SoftDelete } from "./softDelete";
